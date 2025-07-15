5013c9e6c9df82cee932251664c8f76067b28a8c
Christy Maria <93939468+chriz-ty@users.noreply.github.com>
2025-07-15 19:52:35 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
               2000,
               'Web Designer',
               2000,
-              'B. Tech Student',
+              'B. Tech Graduate',
               2000
             ]}
             wrapper="span"
